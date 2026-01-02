baae35a2cc1ae6e7cde3e5593436a3268cf1da1d
Mursheda Urme <urmemursheda@gmail.com>
2026-01-03 00:26:22 +0600

    up

--- bin/module_6/Mobile.dart
+++ bin/module_6/Mobile.dart
@@ -1,4 +1,17 @@
 import 'oop3.dart';
 
+main() {
+  Mobile iphone17 = Mobile("iphone 17 pro","Orange");
+  iphone17.takePhoto();
+  iphone17.decoration();
 
-Mobile iphone17 = Mobile();
+  print(iphone17.getprice());
+  print(iphone17.mobileprice);
+
+  ///price change korar por
+  iphone17.setMobileprice(45);
+  print(iphone17.currentPrice);
+
+
+}
+
--- bin/module_6/oop3.dart
+++ bin/module_6/oop3.dart
@@ -1,5 +1,54 @@
- class Mobile {
-  Mobile(){         //constractor, method
+ ///class er 2ta part thakte pare method, properties
+
+class Mobile {
+  Mobile(String modle,String color ){         //constractor, method, requaired keyword
     print('creating new object');
+
+this.modle=modle;
+this.color=color;
+
+
   }
+
+  ///properties/ atteibuties
+  double _price= 2343;         ///private     keu excess korte parbena encapsulation process
+  String modle = "";
+  String? year;
+  String?color;
+  /// function jokhon class er vitor thake tokhon sethake methode bola hoy
+  void takePhoto (){                /// method
+    print("$modle is capturing photo");
+    print("$modle is photo");
+
+
+  }  void decoration(){                /// method
+    print("$color is standard");
+    print("$_price is high");
+
+print("$_price");
+
+
+  }
+
+  ///pure get method    private value er jnne use hoy
+  double getprice(){
+    return _price;
+  }
+  ///pure get method
+
+  double  get mobileprice {
+    return _price;
+  }
+
+  ///future e kokhono price set korte parar jnne
+ void setMobileprice(double newPrice){
+    if (newPrice >=0) {
+      _price = newPrice;
+    }else{
+      throw Exception("not working");
+    }
+ }
+ double get currentPrice{
+    return _price;
+ }
  }
